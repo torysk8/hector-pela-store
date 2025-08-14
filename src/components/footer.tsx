a63aad514bacf9b736a551f31b5fb69ce242b130
torysk8 <208693110+torysk8@users.noreply.github.com>
2025-08-14 02:26:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+    return (
+        <footer className="bg-black py-4 text-center text-sm text-white border-t">
+          
+            <p>© {new Date().getFullYear()} Todos los derechos reservados</p>
+            <p>Desarrollado por <a href="https://deepfc.vercel.app/" target="_blank" rel="noopener" className="text-blue-500 hover:underline">DeepFC</a></p>
+        </footer>
+    )
+}
